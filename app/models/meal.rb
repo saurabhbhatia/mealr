0bc7e80c17288862330884e78b2b79380a039606
saurabhbhatia <saurabh.a.bhatia@gmail.com>
2014-07-28 11:36:50 +0800

    tests passing for validation

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,2 +1,3 @@
 class Meal < ActiveRecord::Base
+	validates :name, presence: true
 end
--- test/models/meal_test.rb
+++ test/models/meal_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
+require 'minitest/autorun'
 
 class MealTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	def valid_params
+		{ name: "Pizza", chef_id: 1, meal_type_id: 1, food_preference_id: 1, availability: Time.now }
+	end
+
+	test "should be a valid_meal" do
+		meal = Meal.new valid_params
+		assert meal.valid?, "Can't create with valid params: #{meal.errors.messages}"
+	end
+
+	test "not_valid_without_name" do
+		params = valid_params.clone
+    	params.delete :name
+    	meal = Meal.new params
+
+    	refute meal.valid?, "Can't be valid without name"
+    	assert meal.errors[:name].present?, "Missing error when without name"
+	end
 end
